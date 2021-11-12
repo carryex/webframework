770232ec24346f5a4525909d66abe6874441fe37
Daniil Pokinsokha <carryex@returns.co>
2021-11-12 08:00:12 +0100

    Add new class Collection

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
+import { Collection } from './Collections';
 interface UserProps {
   id?: number;
   name?: string;
@@ -10,13 +11,18 @@ interface UserProps {
 const rootUrl = 'http://localhost:3000/users';
 
 class User extends Model<UserProps> {
-  static buildUser(attrs: UserProps): User {
+  static buildUser = (attrs: UserProps): User => {
     return new User(
       new Attributes<UserProps>(attrs),
       new ApiSync<UserProps>(rootUrl),
       new Eventing()
     );
-  }
+  };
+
+  static buildUserCollection = (): Collection<User, UserProps> =>
+    new Collection<User, UserProps>(rootUrl, User.buildUser);
+
+  isAdminUser = (): boolean => this.get('id') === 1;
 }
 
-export { User };
+export { User, UserProps };
